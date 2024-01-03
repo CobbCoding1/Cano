7148bd011221de87b36218021f2e2dd1875a4e2d
isaac cobb <ic@ic-debian-macbook>
2024-01-03 16:40:31 -0500

    got basic save functionality working kinda

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <curses.h>
 
 #define ctrl(x) ((x) & 0x1f)
 
-#define BACKSPACE 263
+#define BACKSPACE 127 
 #define ESCAPE    27
 
 typedef enum {
@@ -12,6 +13,7 @@ typedef enum {
 } Mode;
 
 Mode mode = NORMAL;
+int QUIT = 0;
 
 char *stringify_mode() {
     switch(mode) {
@@ -33,6 +35,9 @@ int main(void) {
     keypad(stdscr, TRUE);
     noecho();
 
+    char *buf = malloc(sizeof(char) * 1024);
+    size_t buf_s = 0;
+
     int row, col;
     getmaxyx(stdscr, row, col);
 
@@ -42,7 +47,8 @@ int main(void) {
     int ch = 0;
 
     int x, y = 0;
-    while(ch != ctrl('q')) {
+    while(ch != ctrl('q') && QUIT != 1) {
+        refresh();
         mvprintw(row-1, 0, stringify_mode());
         move(y, x);
         ch = getch();
@@ -50,6 +56,11 @@ int main(void) {
             case NORMAL:
                 if(ch == 'i') {
                     mode = INSERT;
+                } else if(ch == ctrl('s')) {
+                    FILE *file = fopen("out.txt", "w"); 
+                    fwrite(buf, buf_s, 1, file);
+                    fclose(file);
+                    QUIT = 1;
                 }
                 break;
             case INSERT: {
@@ -58,10 +69,12 @@ int main(void) {
                     getyx(stdscr, y, x);
                     move(y, x-1);
                     delch();
+                    buf[buf_s--] = ' ';
                 } else if(ch == ESCAPE) {
                     mode = NORMAL;
                     keypad(stdscr, TRUE);
                 } else {
+                    buf[buf_s++] = ch;
                     addch(ch);
                 }
                 break;
