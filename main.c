c562761eefc4f7695243e635d52152d532c494f7
isaac cobb <ic@ic-debian-macbook>
2024-01-03 18:05:55 -0500

    did a lot of stuff. todo: update the cur_index when using jk to maneuveur

--- main.c
+++ main.c
@@ -6,11 +6,26 @@
 
 #define BACKSPACE 127 
 #define ESCAPE    27
+#define ENTER     10
 
 typedef enum {
     NORMAL,
     INSERT,
 } Mode;
+
+typedef struct {
+    size_t index;
+    size_t size;
+    char *contents;
+} Row;
+
+typedef struct {
+    char *buf;
+    Row rows[1024];
+    size_t row_index;
+    size_t cur_pos;
+    size_t row_s;
+} Buffer;
 
 Mode mode = NORMAL;
 int QUIT = 0;
@@ -35,10 +50,13 @@ int main(void) {
     keypad(stdscr, TRUE);
     noecho();
 
-    char *buf = malloc(sizeof(char) * 1024);
-    size_t buf_s = 0;
+    Buffer buffer = {0};
+    for(size_t i = 0; i < 1024; i++) {
+        buffer.rows[i].contents = calloc(1024, sizeof(char));
+    }
 
     int row, col;
+    (void)col;
     getmaxyx(stdscr, row, col);
 
     mvprintw(row-1, 0, stringify_mode());
@@ -50,15 +68,38 @@ int main(void) {
     while(ch != ctrl('q') && QUIT != 1) {
         refresh();
         mvprintw(row-1, 0, stringify_mode());
+        
+        for(size_t i = 0; i <= buffer.row_s; i++) {
+            mvprintw(i, 0, buffer.rows[i].contents);
+        }
+
         move(y, x);
         ch = getch();
         switch(mode) {
             case NORMAL:
                 if(ch == 'i') {
                     mode = INSERT;
+                } else if(ch == 'h') {
+                    move(y, x-1);
+                    buffer.cur_pos -= 1;
+                } else if(ch == 'l') {
+                    move(y, x+1);
+                    buffer.cur_pos += 1;
+                } else if(ch == 'k') {
+                    if(x > (int)buffer.rows[--buffer.row_index].size - 1) {
+                        x = buffer.rows[buffer.row_index].size - 1;
+                    }
+                    move(y-1, x);
+                } else if(ch == 'j') {
+                    if(x > (int)buffer.rows[++buffer.row_index].size - 1) {
+                        x = buffer.rows[buffer.row_index].size - 1;
+                    }
+                    move(y+1, x);
                 } else if(ch == ctrl('s')) {
                     FILE *file = fopen("out.txt", "w"); 
-                    fwrite(buf, buf_s, 1, file);
+                    for(size_t i = 0; i <= buffer.row_s; i++ ) {
+                        fwrite(buffer.rows[i].contents, buffer.rows[i].size, 1, file);
+                    }
                     fclose(file);
                     QUIT = 1;
                 }
@@ -67,15 +108,32 @@ int main(void) {
                 keypad(stdscr, FALSE);
                 if(ch == BACKSPACE) {
                     getyx(stdscr, y, x);
-                    move(y, x-1);
-                    delch();
-                    buf[buf_s--] = ' ';
+                    if(buffer.cur_pos == 0) {
+                        if(buffer.row_index != 0) {
+                            Row *cur = &buffer.rows[--buffer.row_index];
+                            buffer.cur_pos = cur->size;
+                            move(buffer.row_index, buffer.cur_pos);
+                        }
+                    } else {
+                        Row *cur = &buffer.rows[buffer.row_index];
+                        cur->contents[--buffer.cur_pos] = ' ';
+                        cur->size = buffer.cur_pos;
+                        move(y, buffer.cur_pos);
+                    }
                 } else if(ch == ESCAPE) {
                     mode = NORMAL;
                     keypad(stdscr, TRUE);
+                } else if(ch == ENTER) {
+                    buffer.rows[buffer.row_index].contents[buffer.rows[buffer.row_index].size++] = '\n';
+                    buffer.row_index++; 
+                    buffer.row_s++;
+                    buffer.cur_pos = 0;
+                    move(buffer.row_index, buffer.cur_pos);
                 } else {
-                    buf[buf_s++] = ch;
-                    addch(ch);
+                    Row *cur = &buffer.rows[buffer.row_index];
+                    cur->contents[buffer.cur_pos++] = ch;
+                    cur->size = buffer.cur_pos;
+                    move(y, buffer.cur_pos);
                 }
                 break;
              }
