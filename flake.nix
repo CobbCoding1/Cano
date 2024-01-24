a8c01a531daaba51ecebae273c5caf78dae40a0d
Sigmanificient <edhyjox@gmail.com>
2024-01-24 16:05:01 +0100

    harmonize devShell & package setting (hardening) + format

--- flake.nix
+++ flake.nix
@@ -5,27 +5,35 @@
   };
 
   outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in rec {
-      devShell = pkgs.mkShell {
-        inputsFrom = [ packages.cano ];
-      };
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-      packages = rec {
-        default = cano;
-        cano = pkgs.stdenv.mkDerivation {
-          name = "cano";
-          src = ./.;
+        hardeningDisable = [ "format" "fortify" ];
+      in
+      rec {
+        devShell = pkgs.mkShell {
+          inherit hardeningDisable;
 
-          buildInputs = with pkgs; [ gnumake ncurses ];
-          hardeningDisable = [ "format" "fortify" ];
-
-          installPhase = ''
-            mkdir -p $out/bin
-            cp build/cano $out/bin
-          '';
+          inputsFrom = [ packages.cano ];
         };
-      };
-    });
+
+        formatter = pkgs.nixpkgs-fmt;
+
+        packages = rec {
+          default = cano;
+          cano = pkgs.stdenv.mkDerivation rec {
+            inherit hardeningDisable;
+
+            name = "cano";
+            src = ./.;
+
+            buildInputs = with pkgs; [ gnumake ncurses ];
+            installPhase = ''
+              mkdir -p $out/bin
+              cp build/${name} $out/bin
+            '';
+          };
+        };
+      });
 }
